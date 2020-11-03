4665e39735267c063ab1fd9f55d954443a1b8c3b
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-03 16:24:24 -0600

    added notes/pseudo code to start || started questions object

---
+++ Assets/script.js
@@ -0,0 +1,55 @@
+//IDEA
+// ---------------------------------------------------
+
+// var numberArray = ["john", "doe", "jane"];
+// // console.log(numberArray.length);
+//     for(var i = 0; i < numberArray.length; i++) {
+//         console.log([i]);
+//     }
+
+
+// var actor = {
+//     name: "john",
+//     age: 9,
+//     lastName: "doe",
+//     isSleeping: true
+// }
+
+// console.log(cat);
+
+// ---------------------------------------------------
+// ---- step 1: Start with an Function
+// var QuestionArray = {}
+
+// ---- step 2: add an Array of questions within the function
+// var QuestionArray = {
+//     Questions: [Question1, Question2, Question3, Question4, Question5]
+// }
+
+// ---- step 3: per index (within the array), add an object in each 
+// var QuestionArray = {
+//     Questions: [{},{},{},{},{}]
+// }
+// -----------------------------------------------------
+
+// ---- START OF CODE ---- //
+
+var questionArray = {
+    questions: [
+        {
+            question: "What is my name?",
+            choices: ["Ivy", "Jane", "Sarah", "Kate"],
+            answer: 0,
+            id: "question-one"
+            
+        }, 
+        {
+            
+        },{},{},{}]
+} 
+    
+    console.log(questionArray);
+
+
+
+
